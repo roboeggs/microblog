f5305abac3921fb752ee968f73e54d1e41d81a4b
Roboeggs <andfishka@gmail.com>
2024-07-11 18:43:17 +0300

    feat: create a registration page for a new user

--- db_schema.sql
+++ db_schema.sql
@@ -1,4 +1,3 @@
-
 -- This makes sure that foreign_key constraints are observed and that errors will be thrown for violations
 PRAGMA foreign_keys=ON;
 
@@ -8,12 +7,15 @@ BEGIN TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS users (
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_name TEXT NOT NULL
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS email_accounts (
     email_account_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email_address TEXT NOT NULL,
+    email_address TEXT NOT NULL UNIQUE,
     user_id  INT, --the user that the email account belongs to
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
@@ -21,14 +23,13 @@ CREATE TABLE IF NOT EXISTS email_accounts (
 -- Insert default data (if necessary here)
 
 -- Set up three users
-INSERT INTO users ('user_name') VALUES ('Simon Star');
-INSERT INTO users ('user_name') VALUES ('Dianne Dean');
-INSERT INTO users ('user_name') VALUES ('Harry Hilbert');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Simon', 'Star', 'simon@gmail.com', 'password1');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Dianne', 'Dean', 'dianne@yahoo.co.uk', 'password2');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Harry', 'Hilbert', 'harry@hotmail.com', 'password3');
 
 -- Give Simon two email addresses and Diane one, but Harry has none
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@gmail.com', 1); 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@hotmail.com', 1); 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('dianne@yahoo.co.uk', 2); 
+INSERT INTO email_accounts (email_address, user_id) VALUES ('simon@gmail.com', 1); 
+INSERT INTO email_accounts (email_address, user_id) VALUES ('simon@hotmail.com', 1); 
+INSERT INTO email_accounts (email_address, user_id) VALUES ('dianne@yahoo.co.uk', 2); 
 
 COMMIT;
-
--- routes/users.js
+++ routes/users.js
@@ -42,21 +42,30 @@ router.get("/add-user", (req, res) => {
  */
 router.post("/add-user", (req, res, next) => {
     // Define the query
-    query = "INSERT INTO users (user_name) VALUES( ? );"
-    query_parameters = [req.body.user_name]
+    const query = "INSERT INTO users (first_name, last_name, email, password) VALUES(?, ?, ?, ?);"
+    
+    // Get the form data
+    const { first_name, last_name, email, psw } = req.body;
+
+    // Validate the form data here before inserting into the database
+
+    const query_parameters = [first_name, last_name, email, psw]
+    console.log(query_parameters);
     
     // Execute the query and send a confirmation message
     global.db.run(query, query_parameters,
         function (err) {
             if (err) {
-                next(err); //send the error on to the error handler
-            } else {
-                res.send(`New data inserted @ id ${this.lastID}!`);
-                next();
+                console.error(err.message); // Log error message in your server's console
+                res.status(400).send({ error: err.message }); // Send error message as a response from the server
+                return;
             }
+            // If everything went well, send the new user's ID as a response from the server
+            res.status(201).send({ user_id: this.lastID });
         }
     );
 });
+
 
 // Export the router object so index.js can access it
 module.exports = router;
