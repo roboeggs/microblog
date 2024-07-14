7ef1611bcb1c8f324d1332971e895a17fe07eb28
Roboeggs <andfishka@gmail.com>
2024-07-15 02:28:58 +0300

    fix: add sorting of articles and posts

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -3,6 +3,7 @@ const dbUtils = require('../utils/dbUtils');
 const asyncHandler = require('../utils/asyncHandler');
 
 const articleController = {
+  /** 
   getArticles: asyncHandler(async (req, res) => {
     const articles = await dbUtils.all("SELECT * FROM articles WHERE status = 'pub';");
 
@@ -29,6 +30,35 @@ const articleController = {
       timeAgo: helpers.timeAgo
     });
   }),
+  */
+
+  getArticles: asyncHandler(async (req, res) => {
+    // Изменяем SQL-запрос, добавляя сортировку по дате создания (предполагая, что у вас есть поле created_at)
+    const articles = await dbUtils.all("SELECT * FROM articles WHERE status = 'pub' ORDER BY created DESC;");
+  
+    const articlesWithDetails = await Promise.all(articles.map(async (article) => {
+      const userQuery = `SELECT blog, first_name, last_name FROM users WHERE user_id = ?;`;
+      const commentQuery = `SELECT COUNT(*) as commentCount FROM comments WHERE article_id = ?;`;
+  
+      const [user, comment] = await Promise.all([
+        dbUtils.get(userQuery, [article.user_id]),
+        dbUtils.get(commentQuery, [article.article_id])
+      ]);
+  
+      return {
+        ...article,
+        name: { blog: user.blog, first: user.first_name, last: user.last_name },
+        commentCount: comment.commentCount
+      };
+    }));
+  
+    res.render(res.locals.layout, {
+      title: 'Small Blog',
+      content: 'articles',
+      articles: articlesWithDetails,
+      timeAgo: helpers.timeAgo
+    });
+  }),
 
   getArticle: asyncHandler(async (req, res) => {
     const articleId = req.params.id;
@@ -37,7 +67,8 @@ const articleController = {
       SELECT comments.comment, comments.comment_id, comments.created, users.first_name, users.last_name 
       FROM comments 
       JOIN users ON comments.user_id = users.user_id 
-      WHERE comments.article_id = ?;
+      WHERE comments.article_id = ?
+      ORDER BY comments.created DESC;
     `;
     const updateViewsQuery = `UPDATE articles SET views = views + 1 WHERE article_id = ?;`;
 
