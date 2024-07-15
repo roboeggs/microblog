ef53420d4fec12012d2190e1c84555be7f6d526b
Roboeggs <andfishka@gmail.com>
2024-07-15 09:10:06 +0300

    fix: bag

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,13 +5,13 @@ const SECRET_KEY = 'small-blog-key';    // defining a secret key for signing the
 const verifyToken = (req, res, next) => {
     const token = req.cookies.token;    // Extracting the token from the cookies
     if (!token) {   // If there's no token, redirect the user to the login page
-        return res.redirect('/users/login');
+        return res.redirect('/author/login');
     }
     // Verifying the token using the secret key
     jwt.verify(token, SECRET_KEY, (err, user) => {
         // If there's an error (token is invalid or expired), redirect the user to the login page
         if (err) {
-            return res.redirect('/users/login');
+            return res.redirect('/author/login');
         }
         // If the token is valid, attach the user object to the request
         req.user = user;
